c0c4a8354daf5dd1d7e0d0179fe5b2bbdb3a6281
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-01-07 13:04:34 -0300

    Fix lexer tests

--- tests/lexer.rs
+++ tests/lexer.rs
@@ -15,122 +15,122 @@ fn test_lexer_should_lexer_single_tokens() {
     let tokens = vec![
         Token::new(
             TokenType::Colon,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             ":".to_string(),
         ),
         Token::new(
             TokenType::Comma,
-            Location::new(1, 1, Rc::clone(&filename)),
+            Location::new(1, 2, Rc::clone(&filename)),
             ",".to_string(),
         ),
         Token::new(
             TokenType::Dot,
-            Location::new(1, 2, Rc::clone(&filename)),
+            Location::new(1, 3, Rc::clone(&filename)),
             ".".to_string(),
         ),
         Token::new(
             TokenType::Plus,
-            Location::new(1, 3, Rc::clone(&filename)),
+            Location::new(1, 4, Rc::clone(&filename)),
             "+".to_string(),
         ),
         Token::new(
             TokenType::Minus,
-            Location::new(1, 4, Rc::clone(&filename)),
+            Location::new(1, 5, Rc::clone(&filename)),
             "-".to_string(),
         ),
         Token::new(
             TokenType::Slash,
-            Location::new(1, 5, Rc::clone(&filename)),
+            Location::new(1, 6, Rc::clone(&filename)),
             "/".to_string(),
         ),
         Token::new(
             TokenType::Asterisk,
-            Location::new(1, 6, Rc::clone(&filename)),
+            Location::new(1, 7, Rc::clone(&filename)),
             "*".to_string(),
         ),
         Token::new(
             TokenType::Greater,
-            Location::new(1, 8, Rc::clone(&filename)),
+            Location::new(1, 9, Rc::clone(&filename)),
             ">".to_string(),
         ),
         Token::new(
             TokenType::Less,
-            Location::new(1, 9, Rc::clone(&filename)),
+            Location::new(1, 10, Rc::clone(&filename)),
             "<".to_string(),
         ),
         Token::new(
             TokenType::Assign,
-            Location::new(1, 11, Rc::clone(&filename)),
+            Location::new(1, 12, Rc::clone(&filename)),
             "=".to_string(),
         ),
         Token::new(
             TokenType::Question,
-            Location::new(1, 12, Rc::clone(&filename)),
+            Location::new(1, 13, Rc::clone(&filename)),
             "?".to_string(),
         ),
         Token::new(
             TokenType::Caret,
-            Location::new(1, 13, Rc::clone(&filename)),
+            Location::new(1, 14, Rc::clone(&filename)),
             "^".to_string(),
         ),
         Token::new(
             TokenType::BitWiseAnd,
-            Location::new(1, 14, Rc::clone(&filename)),
+            Location::new(1, 15, Rc::clone(&filename)),
             "&".to_string(),
         ),
         Token::new(
             TokenType::Pipe,
-            Location::new(1, 15, Rc::clone(&filename)),
+            Location::new(1, 16, Rc::clone(&filename)),
             "|".to_string(),
         ),
         Token::new(
             TokenType::Semicolon,
-            Location::new(2, 0, Rc::clone(&filename)),
+            Location::new(2, 1, Rc::clone(&filename)),
             ";".to_string(),
         ),
         Token::new(
             TokenType::LBrace,
-            Location::new(2, 1, Rc::clone(&filename)),
+            Location::new(2, 2, Rc::clone(&filename)),
             "{".to_string(),
         ),
         Token::new(
             TokenType::RBrace,
-            Location::new(2, 2, Rc::clone(&filename)),
+            Location::new(2, 3, Rc::clone(&filename)),
             "}".to_string(),
         ),
         Token::new(
             TokenType::LParen,
-            Location::new(2, 3, Rc::clone(&filename)),
+            Location::new(2, 4, Rc::clone(&filename)),
             "(".to_string(),
         ),
         Token::new(
             TokenType::RParen,
-            Location::new(2, 4, Rc::clone(&filename)),
+            Location::new(2, 5, Rc::clone(&filename)),
             ")".to_string(),
         ),
         Token::new(
             TokenType::LBracket,
-            Location::new(2, 5, Rc::clone(&filename)),
+            Location::new(2, 6, Rc::clone(&filename)),
             "[".to_string(),
         ),
         Token::new(
             TokenType::RBracket,
-            Location::new(2, 6, Rc::clone(&filename)),
+            Location::new(2, 7, Rc::clone(&filename)),
             "]".to_string(),
         ),
         Token::new(
             TokenType::Bang,
-            Location::new(2, 7, Rc::clone(&filename)),
+            Location::new(2, 8, Rc::clone(&filename)),
             "!".to_string(),
         ),
         Token::new(
             TokenType::Mod,
-            Location::new(2, 8, Rc::clone(&filename)),
+            Location::new(2, 9, Rc::clone(&filename)),
             "%".to_string(),
         ),
         Token::new(
             TokenType::Eof,
-            Location::new(2, 9, Rc::clone(&filename)),
+            Location::new(2, 10, Rc::clone(&filename)),
             "\0".to_string(),
         ),
     ];
@@ -150,42 +150,42 @@ mila.lang
     let tokens = vec![
         Token::new(
             TokenType::Identifier,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "mila_lang".to_string(),
         ),
         Token::new(
             TokenType::Identifier,
-            Location::new(2, 0, Rc::clone(&filename)),
+            Location::new(2, 1, Rc::clone(&filename)),
             "mila_lang2".to_string(),
         ),
         Token::new(
             TokenType::Identifier,
-            Location::new(3, 0, Rc::clone(&filename)),
+            Location::new(3, 1, Rc::clone(&filename)),
             "_mila_lang".to_string(),
         ),
         Token::new(
             TokenType::Identifier,
-            Location::new(3, 11, Rc::clone(&filename)),
+            Location::new(3, 12, Rc::clone(&filename)),
             "mila_lang".to_string(),
         ),
         Token::new(
             TokenType::Identifier,
-            Location::new(4, 0, Rc::clone(&filename)),
+            Location::new(4, 1, Rc::clone(&filename)),
             "mila".to_string(),
         ),
         Token::new(
             TokenType::Dot,
-            Location::new(4, 4, Rc::clone(&filename)),
+            Location::new(4, 5, Rc::clone(&filename)),
             ".".to_string(),
         ),
         Token::new(
             TokenType::Identifier,
-            Location::new(4, 5, Rc::clone(&filename)),
+            Location::new(4, 6, Rc::clone(&filename)),
             "lang".to_string(),
         ),
         Token::new(
             TokenType::Illegal,
-            Location::new(5, 0, Rc::clone(&filename)),
+            Location::new(5, 1, Rc::clone(&filename)),
             "#aaaa".to_string(),
         ),
     ];
@@ -207,92 +207,92 @@ fn test_lexer_number_token() {
     let tokens = vec![
         Token::new(
             TokenType::Number,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "12345".to_string(),
         ),
         Token::new(
             TokenType::Semicolon,
-            Location::new(1, 5, Rc::clone(&filename)),
+            Location::new(1, 6, Rc::clone(&filename)),
             ";".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(1, 6, Rc::clone(&filename)),
+            Location::new(1, 7, Rc::clone(&filename)),
             "123456".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(2, 0, Rc::clone(&filename)),
+            Location::new(2, 1, Rc::clone(&filename)),
             "123456789".to_string(),
         ),
         Token::new(
             TokenType::Greater,
-            Location::new(2, 9, Rc::clone(&filename)),
+            Location::new(2, 10, Rc::clone(&filename)),
             ">".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(3, 0, Rc::clone(&filename)),
+            Location::new(3, 1, Rc::clone(&filename)),
             "123456".to_string(),
         ),
         Token::new(
             TokenType::Less,
-            Location::new(3, 6, Rc::clone(&filename)),
+            Location::new(3, 7, Rc::clone(&filename)),
             "<".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(3, 7, Rc::clone(&filename)),
+            Location::new(3, 8, Rc::clone(&filename)),
             "458444".to_string(),
         ),
         Token::new(
             TokenType::Pipe,
-            Location::new(3, 13, Rc::clone(&filename)),
+            Location::new(3, 14, Rc::clone(&filename)),
             "|".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(4, 0, Rc::clone(&filename)),
+            Location::new(4, 1, Rc::clone(&filename)),
             "121221".to_string(),
         ),
         Token::new(
             TokenType::BitWiseAnd,
-            Location::new(4, 6, Rc::clone(&filename)),
+            Location::new(4, 7, Rc::clone(&filename)),
             "&".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(5, 0, Rc::clone(&filename)),
+            Location::new(5, 1, Rc::clone(&filename)),
             "125478".to_string(),
         ),
         Token::new(
             TokenType::Caret,
-            Location::new(5, 6, Rc::clone(&filename)),
+            Location::new(5, 7, Rc::clone(&filename)),
             "^".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(6, 0, Rc::clone(&filename)),
+            Location::new(6, 1, Rc::clone(&filename)),
             "1".to_string(),
         ),
         Token::new(
             TokenType::Comma,
-            Location::new(6, 1, Rc::clone(&filename)),
+            Location::new(6, 2, Rc::clone(&filename)),
             ",".to_string(),
         ),
         Token::new(
             TokenType::Number,
-            Location::new(6, 2, Rc::clone(&filename)),
+            Location::new(6, 3, Rc::clone(&filename)),
             "2".to_string(),
         ),
         Token::new(
             TokenType::Illegal,
-            Location::new(7, 0, Rc::clone(&filename)),
+            Location::new(7, 1, Rc::clone(&filename)),
             "1477a".to_string(),
         ),
         Token::new(
             TokenType::Semicolon,
-            Location::new(7, 5, Rc::clone(&filename)),
+            Location::new(7, 6, Rc::clone(&filename)),
             ";".to_string(),
         ),
     ];
@@ -312,32 +312,32 @@ fn test_lexer_floating_pointer_token() {
     let tokens = vec![
         Token::new(
             TokenType::FloatingPointNumber,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "100.0".to_string(),
         ),
         Token::new(
             TokenType::FloatingPointNumber,
-            Location::new(2, 0, Rc::clone(&filename)),
+            Location::new(2, 1, Rc::clone(&filename)),
             "100.0".to_string(),
         ),
         Token::new(
             TokenType::Semicolon,
-            Location::new(2, 5, Rc::clone(&filename)),
+            Location::new(2, 6, Rc::clone(&filename)),
             ";".to_string(),
         ),
         Token::new(
             TokenType::Illegal,
-            Location::new(3, 0, Rc::clone(&filename)),
+            Location::new(3, 1, Rc::clone(&filename)),
             "100.0a".to_string(),
         ),
         Token::new(
             TokenType::FloatingPointNumber,
-            Location::new(4, 0, Rc::clone(&filename)),
+            Location::new(4, 1, Rc::clone(&filename)),
             "100.1".to_string(),
         ),
         Token::new(
             TokenType::Illegal,
-            Location::new(5, 0, Rc::clone(&filename)),
+            Location::new(5, 1, Rc::clone(&filename)),
             "100.0.0".to_string(),
         ),
     ];
@@ -352,37 +352,37 @@ fn test_word_token() {
     let tokens = vec![
         Token::new(
             TokenType::Let,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "let".to_string(),
         ),
         Token::new(
             TokenType::Var,
-            Location::new(1, 4, Rc::clone(&filename)),
+            Location::new(1, 5, Rc::clone(&filename)),
             "var".to_string(),
         ),
         Token::new(
             TokenType::While,
-            Location::new(1, 8, Rc::clone(&filename)),
+            Location::new(1, 9, Rc::clone(&filename)),
             "while".to_string(),
         ),
         Token::new(
             TokenType::True,
-            Location::new(1, 14, Rc::clone(&filename)),
+            Location::new(1, 15, Rc::clone(&filename)),
             "true".to_string(),
         ),
         Token::new(
             TokenType::False,
-            Location::new(1, 19, Rc::clone(&filename)),
+            Location::new(1, 20, Rc::clone(&filename)),
             "false".to_string(),
         ),
         Token::new(
             TokenType::Fn,
-            Location::new(1, 25, Rc::clone(&filename)),
+            Location::new(1, 26, Rc::clone(&filename)),
             "fn".to_string(),
         ),
         Token::new(
             TokenType::Ret,
-            Location::new(1, 28, Rc::clone(&filename)),
+            Location::new(1, 29, Rc::clone(&filename)),
             "ret".to_string(),
         ),
     ];
@@ -396,62 +396,62 @@ fn test_two_char_token() {
     let tokens = vec![
         Token::new(
             TokenType::Eq,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "==".to_string(),
         ),
         Token::new(
             TokenType::NotEq,
-            Location::new(1, 3, Rc::clone(&filename)),
+            Location::new(1, 4, Rc::clone(&filename)),
             "!=".to_string(),
         ),
         Token::new(
             TokenType::LessThanOrEq,
-            Location::new(1, 6, Rc::clone(&filename)),
+            Location::new(1, 7, Rc::clone(&filename)),
             "<=".to_string(),
         ),
         Token::new(
             TokenType::GreaterThanOrEq,
-            Location::new(1, 9, Rc::clone(&filename)),
+            Location::new(1, 10, Rc::clone(&filename)),
             "<=".to_string(),
         ),
         Token::new(
             TokenType::ShiftRight,
-            Location::new(1, 12, Rc::clone(&filename)),
+            Location::new(1, 13, Rc::clone(&filename)),
             ">>".to_string(),
         ),
         Token::new(
             TokenType::ShiftLeft,
-            Location::new(1, 15, Rc::clone(&filename)),
+            Location::new(1, 16, Rc::clone(&filename)),
             "<<".to_string(),
         ),
         Token::new(
             TokenType::PlusAssign,
-            Location::new(1, 18, Rc::clone(&filename)),
+            Location::new(1, 19, Rc::clone(&filename)),
             "+=".to_string(),
         ),
         Token::new(
             TokenType::MinusAssign,
-            Location::new(1, 21, Rc::clone(&filename)),
+            Location::new(1, 22, Rc::clone(&filename)),
             "-=".to_string(),
         ),
         Token::new(
             TokenType::AsteriskAssign,
-            Location::new(1, 24, Rc::clone(&filename)),
+            Location::new(1, 25, Rc::clone(&filename)),
             "*=".to_string(),
         ),
         Token::new(
             TokenType::SlashAssign,
-            Location::new(1, 27, Rc::clone(&filename)),
+            Location::new(1, 28, Rc::clone(&filename)),
             "/=".to_string(),
         ),
         Token::new(
             TokenType::And,
-            Location::new(1, 30, Rc::clone(&filename)),
+            Location::new(1, 31, Rc::clone(&filename)),
             "&&".to_string(),
         ),
         Token::new(
             TokenType::Or,
-            Location::new(1, 33, Rc::clone(&filename)),
+            Location::new(1, 34, Rc::clone(&filename)),
             "||".to_string(),
         ),
     ];
@@ -470,22 +470,22 @@ fn test_string_token() {
     let tokens = vec![
         Token::new(
             TokenType::String,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "mila_lang".to_string(),
         ),
         Token::new(
             TokenType::String,
-            Location::new(2, 0, Rc::clone(&filename)),
+            Location::new(2, 1, Rc::clone(&filename)),
             "banana\n".to_string(),
         ),
         Token::new(
             TokenType::Semicolon,
-            Location::new(3, 1, Rc::clone(&filename)),
+            Location::new(3, 2, Rc::clone(&filename)),
             ";".to_string(),
         ),
         Token::new(
             TokenType::Illegal,
-            Location::new(4, 0, Rc::clone(&filename)),
+            Location::new(4, 1, Rc::clone(&filename)),
             "\"aaaaaa".to_string(),
         ),
     ];
@@ -503,17 +503,17 @@ opa"
     let tokens = vec![
         Token::new(
             TokenType::Let,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "let".to_string(),
         ),
         Token::new(
             TokenType::Var,
-            Location::new(2, 0, Rc::clone(&filename)),
+            Location::new(2, 1, Rc::clone(&filename)),
             "var".to_string(),
         ),
         Token::new(
             TokenType::Identifier,
-            Location::new(3, 0, Rc::clone(&filename)),
+            Location::new(3, 1, Rc::clone(&filename)),
             "opa".to_string(),
         ),
     ];
@@ -528,12 +528,12 @@ fn test_if_else_token() {
     let tokens = vec![
         Token::new(
             TokenType::If,
-            Location::new(1, 0, Rc::clone(&filename)),
+            Location::new(1, 1, Rc::clone(&filename)),
             "if".to_string(),
         ),
         Token::new(
             TokenType::Else,
-            Location::new(1, 3, Rc::clone(&filename)),
+            Location::new(1, 4, Rc::clone(&filename)),
             "else".to_string(),
         ),
     ];
