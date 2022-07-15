df6794517307de413ea07ac661c75c43506a6af7
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-07-15 15:51:16 -0300

    impl error for ParseError

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,14 +1,25 @@
 use std::num::{ParseFloatError, ParseIntError};
 
+#[derive(Debug)]
 pub enum ParseError {
     Message(String),
 }
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let msg = match self { Self::Message(msg) => msg };
+        write!(f, "{}", msg)
+    }
+}
+
+impl std::error::Error for ParseError {}
 
 impl From<ParseIntError> for ParseError {
     fn from(e: ParseIntError) -> Self {
         Self::Message(e.to_string())
     }
 }
+
 
 impl From<ParseFloatError> for ParseError {
     fn from(e: ParseFloatError) -> Self {
